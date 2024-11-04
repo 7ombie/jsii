770a1557ec91c7623505650a171a54cf5af44c20
7ombie <carl.joseph.younger@gmail.com>
2024-11-04 12:03:58 +0000

    Renamed 'frontend' to 'console' (to avoid confusion)).

--- source/console/main.js
+++ source/console/main.js
@@ -4,22 +4,15 @@ import { write } from "../compiler/writer.js"
 import { fix } from "../compiler/fixer.js"
 import { put } from "../compiler/helpers.js"
 
-window.lex = lex;
-window.parse = parse;
-window.write = write;
+// window.lex = lex;
+// window.parse = parse;
+// window.write = write;
+
 window.result = await fetch("source.lark");
 window.source = await result.text();
 
 // put("SOURCE...\n---------\n\n" + source);
 // for (const token of lex(source)) put("token:", token);
-// for (const statement of parse(source, {dev: true})) put("statement:", statement);
+// for (const statement of parse(source, {dev: true})) put("basic statement:", statement);
+// for (const statement of fix(source, {dev: true})) put("fixed statement:", statement);
 for (const string of write(source, {dev: true})) put(string);
-
-// function walk(statement) {
-
-//     put("==>", statement);
-
-//     for (const operand of statement) walk(operand);
-// }
-
-// for (const statement of parse(source, {dev: true})) walk(statement);
